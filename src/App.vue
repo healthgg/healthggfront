0e93656965417c39dd748523b9bb7bd4c1149a3d
ystar5008@naver.com <ystar5008@naver.com>
2024-07-31 00:08:25 +0900

    add : 테스트 용 코드 추가

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
+<!-- src/App.vue -->
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <h1>Users List</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">{{ user.id }}</li>
+      <li v-for="user in users" :key="user.name">{{ user.name }}</li>
+    </ul>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+import axios from "axios";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      users: [],
+    };
+  },
+  mounted() {
+    this.fetchUsers();
+  },
+  methods: {
+    async fetchUsers() {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        this.users = response.data;
+      } catch (error) {
+        console.error("There was an error fetching the users:", error);
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
